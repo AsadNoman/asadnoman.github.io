90e951258d1498d7f77ac73ca690fc37639d3c7a
Asad Noman <mrasadnoman@gmail.com>
2018-01-28 14:15:54 +0500

    Near to Final, overview required

---
+++ src/itsme.ts
@@ -0,0 +1,28 @@
+
+import * as ko from "knockout";
+
+class list_item{
+    constructor(public heading: string, public paragraph : string){}
+}
+
+class social_link{
+    constructor(public social: string, public url: string){}
+}
+
+class itsme{
+
+    goodname: string = "ASAD NOMAN";
+    bio: string = "Man with love for the back-end, spend 12-15 hours coding, I fix things";
+    items : list_item[] = [
+        new list_item("Marvel Movies really draw my attention",
+        ":) :) :) :)"),
+        new list_item("I like girls as well", 
+        "that's pretty much natural"),
+        new list_item("sometime it's just fun to code", 
+        "isn't it?"),
+        new list_item("Git for the first time :o", 
+        "OMG, such an awesome experience")
+    ];
+    
+}
+ko.applyBindings(new itsme());
